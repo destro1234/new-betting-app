aa7b846f1bea3eacdd6adec725b453d111d04454
Elias <eliasventura23@gmail.com>
2023-03-20 19:05:31 -0400

    added password_digest to user

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+      <header>
+        <h1>The New Betting App</h1>
+        <h3>Best place to bet NBA!</h3>
+      </header>
+    );
+  }
+  
+  export default Header;
+  
