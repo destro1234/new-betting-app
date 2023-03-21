81e9ace881bf4ad283edd0b60346cbfa107c05f2
Elias <eliasventura23@gmail.com>
2023-03-20 21:03:31 -0400

    add actions to sessions controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import { BrowserRouter as Router, Routes, Route} from 'react-router-dom' 
 
 import Header from './components/Header.js'
@@ -8,15 +8,46 @@ import './App.css';
 
 
 function App() {
-  return (
+
+  const [user, setUser] = useState(null);
+
+  // <Header />
+
+  useEffect(() => {
+    fetch("/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+<Header />
+  if (user) {
+    return ( <React.Fragment>
+      <Header />
+      <h2>Welcome, {user.username}!</h2>
+      </React.Fragment>);
+  } else {
+    return (
     <React.Fragment>
       <Header />
-      <Login />
+      <Login onLogin={setUser} />
     </React.Fragment>
+    );
+  }
+
+
+  // return (
+
+    
+    // <React.Fragment>
+      
+      
+    //   <Login />
+    // </React.Fragment>
 
       
     
-  );
+  // );
 }
 
 export default App;
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   # root 'games#index'
   resources :users, only: [:create, :index, :show]
   resources :games, only: [:index]
+
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get "/me", to: "users#show"
+
 end
